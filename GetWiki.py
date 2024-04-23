5a40316410c657cac3250cded54d9eedc6aaf7d3
Havo <havoeudoera@gmail.com>
2024-04-23 19:29:18 +0200

    Update GetWiki.py

--- GetWiki.py
+++ GetWiki.py
@@ -1,41 +1,51 @@
 import requests
 import re 
 
-def FgetLinks(Surl):
-    RRequest = requests.get(Surl)
-    SHTML = RRequest.text
+def fGetLinks(Surl):
+    rRequest = requests.get(Surl)
+    SHTML = rRequest.text
 
-    LMainBody = str(SHTML)
-    LMainBody = re.split('<div id="bodyContent" class="vector-body" aria-labelledby="firstHeading" data-mw-ve-target-container>', LMainBody)
-    LMainBody = re.split('</main>', LMainBody[1])
-    #LMainBody = LMainBody[0].split('<span class="mw-headline" id="References">References</span>')
-    LMainBody = re.split('<span class="mw-headline" id="References">References</span>', LMainBody[0])
+    lMainBody = str(SHTML)
+    lMainBody = re.split('<div id="bodyContent" class="vector-body" aria-labelledby="firstHeading" data-mw-ve-target-container>', lMainBody)
+    lMainBody = re.split('</main>', lMainBody[1])
+    #lMainBody = lMainBody[0].split('<span class="mw-headline" id="References">References</span>')
+    lMainBody = re.split('<span class="mw-headline" id="References">References</span>', lMainBody[0])
 
-    LHyperlinks = re.findall('<a href="\/wiki\/.*?<\/a>', LMainBody[0])
+    lHyperLinks = re.findall('<a href="/wiki/.*?</a>', lMainBody[0])
 
+    lTitleList, lLinksList = fSortList(lHyperLinks)
+
+    print(lLinksList)
+
+
+def fSortList(lHyperLinks):
     ##lav en liste med ting der skal fjernes
-    LwrongLinks = []
+    lWrongLinks = []
 
-    for i in LHyperlinks:
+    for i in lHyperLinks:
         List =[x for x in i]
         for j in List:
             if j == ':':
-                LwrongLinks.append(i)
+                lWrongLinks.append(i)
                 break
-    LHyperlinks = [x for x in LHyperlinks if x not in LwrongLinks]
-    LtitleList = []
-    for i in LHyperlinks:
-        LtitleList.append(re.findall('title=".*?"', i))
-    
-    for i in range(len(LtitleList)):
-        LtitleList[i] = re.findall(r'"([^"]*)"', str(LtitleList[i]))
-        LtitleList[i] = str(LtitleList[i]).replace("'", "")
-        LtitleList[i] = str(LtitleList[i]).replace("[", "")
-        LtitleList[i] = str(LtitleList[i]).replace("]", "")
+    lHyperLinks = [x for x in lHyperLinks if x not in lWrongLinks]
 
+    lTitleList = []
+    lLinksList = []
 
+    for i in lHyperLinks:
+        lTitleList.append(re.findall('title=".*?"', i))
+        lLinksList.append(re.findall('href=".*?"', i))
 
-    print(LtitleList)
 
     
-FgetLinks('https://en.wikipedia.org/wiki/Wetware_(brain)#Computer_jargon')
+    for i in range(len(lTitleList)):
+        lTitleList[i] = re.findall(r'"([^"]*)"', str(lTitleList[i]))
+        lTitleList[i] = str(lTitleList[i]).replace("'", "").replace("[", "").replace("]", "")
+        lLinksList[i] = re.findall(r'"([^"]*)"', str(lLinksList[i]))
+        lLinksList[i] = str(lLinksList[i]).replace("'", "").replace("[", "").replace("]", "")
+
+    return lTitleList, lLinksList
+    
+    
+fGetLinks('https://en.wikipedia.org/wiki/Wetware_(brain)#Computer_jargon')
