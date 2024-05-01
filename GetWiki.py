3e7bbb08a8d98a7d2a58a5e3463509a2b4fa81b0
Havo <havoeudoera@gmail.com>
2024-05-01 14:29:13 +0200

    Update GetWiki.py

--- GetWiki.py
+++ GetWiki.py
@@ -18,7 +18,7 @@ def fGetArticle(sLink):
         lPages.append("https://en.wikipedia.org/wiki/" + sLink[1])
         return lPages
     else:
-        return "Parameter not supported, use either 'random' or a list of article names"
+        return "Parameter not supported, use either 'random' or a list of two article names"
 
 #Henter HTML koden for en given Wikipedia side, hvorefter der sorteres alle referencer til links ud af hjemmesiden.
 #Input: URL som string
