ba92121c0316b3585e559ba74ab31c95ae419672
ChrVS940 <63612500+ChrVS940@users.noreply.github.com>
2024-05-13 11:37:34 +0200

    rydtet op

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-rURL = requests.get("https://en.wikipedia.org/wiki/Deccan_Plateau")
-bs4Soup = BeautifulSoup(rURL.content, "html.parser")
-sTitle = bs4Soup.find(class_="firstHeading").text
-
-print(sTitle)
